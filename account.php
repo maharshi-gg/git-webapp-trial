5bf3a8a82acbaa988960a31eeaf69ef9c6fb169b
maharshi-gg <maharshig.parekh2014@gmail.com>
2017-03-11 21:05:48 +0530

    Few PHP Files added
    Added various php files.
    Need to make changes to the CSS file - it is horrible (mostly many parts
    are getting overridden)

---
+++ account.php
@@ -0,0 +1,46 @@
+<?php include_once "header.php"; ?>
+<link rel="stylesheet" href="styling.css" type="text/css">
+<div id="main">
+
+   <!-- IF LOGGED IN -->
+
+         <h2>Your Account</h2>
+
+         <form action="">
+            <div>
+               <input type="text" name="username" id="username" />
+               <label for="username">Change Email Address</label>
+
+               <input type="submit" name="change-email-submit" id="change-email-submit" value="Change Email" class="button" />
+            </div>
+         </form>
+
+         <hr />
+
+         <h2>Change Password</h2>
+
+         <form action="#">
+            <div>
+               <label for="password">New Password</label>
+               <input type="password" name="r" id="repeat-new-password" />
+
+               <label for="password">Repeat New Password</label>
+               <input type="submit" name="change-password-submit" id="change-password-submit" value="Change Password" class="button" />
+            </div>
+         </form>
+
+         <hr />
+
+         <form action="" id="delete-account-form">
+             <div>
+                 <input type="submit" name="delete-account-submit" id="delete-account-submit" value="Delete Account?" class="button" />
+             </div>
+         </form>
+
+   <!-- IF LOGGED OUT -->
+
+          <!-- Alternate content here -->
+
+</div>
+
+<?php include_once "sidebar.php"; ?>
---
+++ sidebar.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Strict//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-strict.dtd">
+<link rel="stylesheet" href="CSS/main.css" type="text/css">
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+    <title>Colored Lists | <!-- Do Something Smart Here --></title>
+
+    <link rel="stylesheet" href="styling.css" type="text/css" />
+    <link rel="icon" href="HTML Icons\favicon-checklist.ico" type="image/x-icon">
+
+    <script type='text/javascript' src='//ajax.googleapis.com/ajax/libs/jquery/1.3.2/jquery.min.js?ver=1.3.2'></script>
+</head>
+
+<body>
+        <div id="ribbon">
+
+          Reminders
+
+          <ul>
+               <li>Your list automatically saves</li>
+               <li>Double-click list items to edit them</li>
+          </ul>
+
+        </div>
+</body>
+</html>
